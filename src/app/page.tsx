e521ab5d9dd70794a6925fddfd7fd986b9704ca6
ZhengJieGan <ganzj45@gmail.com>
2023-11-05 00:29:29 +0800

    added redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import { useAppSelector } from "./redux/hooks/hooks";
+import { userState } from "./redux/slices/userSlice";
+
 export default function Home() {
-  return <div>homepage</div>;
+  const user = useAppSelector(userState);
+
+  return <div>{user.name}</div>;
 }
---
+++ src/app/redux/slices/userSlice.tsx
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+import { UserTypes } from "@/app/types/users";
+
+const initialState: UserTypes = {
+  name: "",
+};
+
+export const userSlice = createSlice({
+  name: "users",
+  initialState,
+
+  reducers: {
+    updateUser: (state, action: PayloadAction<UserTypes>) => {
+      state = action.payload;
+    },
+  },
+});
+
+// SAMPLE
+
+// export function getEmployees(dispatch: any) {
+//   API.fetchEmployees()
+//     .then((response) => {
+//       const data = response?.data?.data;
+//       dispatch(updateEmployeeData(data));
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       throw error;
+//     });
+// }
+
+export const { updateUser } = userSlice.actions;
+
+export const userState = (state: RootState) => state.users;
+
+export default userSlice.reducer;
