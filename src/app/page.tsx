2652b8898c9523472790e90c79b195d36494077f
Daniel Yuen <yuendaniel33@gmail.com>
2023-11-13 20:49:18 +0800

    added carousel and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
+import Carousel from "./components/carousel/carousel";
+import { RECOMMENDED_GROUPS } from "./sample/groups";
 
 export default async function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Flex className="flex-col gap-4 px-28 mb-4">
+        <Box className='h-40'>Map goes here</Box>
+        <Carousel groups={RECOMMENDED_GROUPS} />
+      </Flex>
+    </>
+  );
 }
