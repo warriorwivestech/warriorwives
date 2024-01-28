1369c8392840a80928442f56a793bd435e7df1c1
nickfarm27 <nicholasfarm27@gmail.com>
2024-01-28 03:30:13 +0000

    Add read db code

---
+++ src/app/data/read.ts
@@ -0,0 +1,89 @@
+import prisma from "../prisma"
+
+export async function getUserByEmail(email: string) {
+  return await prisma.user.findUnique({
+    where: {
+      email,
+    },
+    include: {
+      groups: true,
+      interests: true,
+    },
+  })
+}
+
+export async function getUserById(id: number) {
+  return await prisma.user.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      groups: true,
+      interests: true,
+    },
+  })
+}
+
+// ? get groups by county/state
+export async function getGroupsByLocationName(location: string) {
+  return await prisma.group.findMany({
+    where: {
+      location
+    },
+    include: {
+      tags: true,
+    },
+  })
+}
+
+export async function getGroupsByInterestId(id: number) {
+  return await prisma.group.findMany({
+    where: {
+      tags: {
+        some: {
+          interestId: id,
+        },
+      },
+    },
+    include: {
+      tags: true,
+    },
+  })
+}
+
+export async function getGroupsByInterestName(name: string) {
+  return await prisma.group.findMany({
+    where: {
+      tags: {
+        some: {
+          interest: {
+            name,
+          },
+        },
+      },
+    },
+    include: {
+      tags: true,
+    },
+  })
+}
+
+export async function getEventsByGroupId(groupId: number) {
+  return await prisma.event.findMany({
+    where: {
+      groupId,
+    },
+  })
+}
+
+export async function getEventById(id: number) {
+  return await prisma.event.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      photos: true,
+      materials: true,
+    },
+  })
+}
