f348c71da26d105764f5505df64164c6c8c3841e
Daniel Yuen <yuendaniel33@gmail.com>
2024-01-31 22:44:06 +0800

    revamp my groups page

--- src/app/components/GroupCards/index.tsx
+++ src/app/components/GroupCards/index.tsx
@@ -12,57 +12,99 @@ import {
   HStack,
   Flex,
   Icon,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverArrow,
+  PopoverBody,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { InfoOutlineIcon } from "@chakra-ui/icons";
+import { RiBaseStationLine } from "react-icons/ri";
 
 interface GroupCardProps {
-  id: string;
-  title: string;
+  id: number;
+  name: string;
   description: string;
-  imageUrl: string;
+  displayPhoto: string;
+  branchOfService: string;
+  county: string;
+  state: string;
+  online: boolean;
+  tags: string[];
 }
 
 export default function GroupCard({
   id,
-  title,
+  name,
   description,
-  imageUrl,
+  displayPhoto,
+  branchOfService,
+  county,
+  state,
+  online,
+  tags,
 }: GroupCardProps) {
   return (
     <Link href={`/groups/${id}`}>
-      <Card className="w-[100%] h-[100%] transition-all duration-300 ease-in-out hover:scale-105">
+      <Card className='w-[100%] h-[100%] transition-all duration-300 ease-in-out hover:bg-gray-50 hover:shadow-lg'>
         <CardBody>
-          {/* displayPhoto */}
-          <Image src={imageUrl} alt={title} borderRadius="lg" />
-          <Stack mt="6" spacing="3">
-            <Flex flexDirection="row" justifyContent="space-between">
-              <HStack spacing={2}>
-                {/* tags */}
-                <Tag w={"auto"}>tags</Tag>
-                <Tag w={"auto"}>tags</Tag>
+          <HStack justifyContent={"space-between"} marginBottom='0.75rem'>
+            <Text>{branchOfService}</Text>
+            <Flex justifyContent='center' gap={2} alignItems='center'>
+              <Icon as={FaMapMarkerAlt} className="text-red-700" />
+              <Text className='whitespace-nowrap text-slate-600 font-semibold'>{`${
+                county && `${county}, `
+              } ${state}`}</Text>
+            </Flex>
+          </HStack>
+          <Image
+            src={displayPhoto}
+            alt={name}
+            borderRadius='lg'
+            className='w-full object-cover h-60'
+          />
+          <Stack mt='6' spacing='3'>
+            <Flex flexDirection='row' justifyContent='space-between'>
+              <HStack spacing={2} className='flex-wrap'>
+                {tags.map((tag) => (
+                  <Tag w={"auto"} className='whitespace-nowrap'>
+                    {tag}
+                  </Tag>
+                ))}
               </HStack>
-              <Flex justifyContent="center" gap={2} alignItems="center">
-                <Icon as={FaMapMarkerAlt} />
-                <Text color="gray.500">Mexico</Text>
-              </Flex>
+              {online && (
+                <Popover trigger='hover' placement='top'>
+                  <PopoverTrigger>
+                    <div>
+                      <RiBaseStationLine className='text-[1.5rem] text-green-500 mr-2 ml-2' />
+                    </div>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverBody>
+                      <Flex
+                        justifyContent={"center"}
+                        alignItems={"center"}
+                        gap={2}
+                      >
+                        <InfoOutlineIcon />
+                        This group is conducted online.
+                      </Flex>
+                    </PopoverBody>
+                  </PopoverContent>
+                </Popover>
+              )}
             </Flex>
             {/* name */}
-            <Heading size="md">{title}</Heading>
+            <Heading size='md'>{name}</Heading>
             {/* description */}
-            <Flex textOverflow="ellipsis" noOfLines={3}>
+            <Flex textOverflow='ellipsis' noOfLines={3}>
               <Text>{description}</Text>
             </Flex>
           </Stack>
         </CardBody>
-        {/* <Divider />
-        <CardFooter>
-          <ButtonGroup spacing="2">
-          <Button className="bg-primary">
-            <Link href={`/groups/${id}`}>View events</Link>
-          </Button>
-        </ButtonGroup>
-        </CardFooter> */}
       </Card>
     </Link>
   );
--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,122 +1,103 @@
 import React from "react";
 import GroupCard from "../components/GroupCards";
-import { SimpleGrid } from "@chakra-ui/react";
+import { Divider, SimpleGrid } from "@chakra-ui/react";
 
 export default function GroupPage() {
   const sampleGroupData = [
     {
-      id: "1",
-      title: "Beach Clean-Up",
-      description:
-        "Join us for a community beach clean-up and make a difference!",
-      imageUrl:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-      buttonText: "Join Event",
+      id: 1,
+      name: "Alpha Team",
+      description: "Special Operations Unit",
+      displayPhoto:
+        "https://t4.ftcdn.net/jpg/03/40/52/49/360_F_340524914_pzOWCq4I0WjytxaW8DTVFujrck1gjvvO.jpg",
+      branchOfService: "Army",
+      county: "XYZ County",
+      state: "California",
+      online: true,
+      members: [],
+      tags: ["Special Operations", "Tactical", "Alpha"],
+      events: [],
     },
     {
-      id: "2",
-      title: "Local Art Exhibition",
-      description:
-        "Explore stunning artworks by local artists in our vibrant art exhibition.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-      buttonText: "View Details",
+      id: 2,
+      name: "Bravo Squadron",
+      description: "Air Support Division",
+      displayPhoto:
+        "https://media.istockphoto.com/id/1370858710/photo/portrait-of-cheerful-mixed-age-range-multi-ethnic-women-celebrating-international-womens-day.jpg?s=612x612&w=0&k=20&c=ViS40J0LzPTcfv-o_bbkDehpdKbC8q-250RD02wmb8c=",
+      branchOfService: "Air Force",
+      county: "ABC County",
+      state: "Texas",
+      online: false,
+      members: [],
+      tags: ["Air Support", "Aviation", "Bravo"],
+      events: [],
     },
     {
-      id: "3",
-      title: "Charity Fun Run",
-      description:
-        "Participate in our 5k fun run to raise funds for a noble cause.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-      buttonText: "Register Now",
+      id: 3,
+      name: "Delta Division",
+      description: "Naval Operations",
+      displayPhoto:
+        "https://media.istockphoto.com/id/951541912/photo/happiness-happens-when-we-stand-together.jpg?s=612x612&w=0&k=20&c=R1qXpX5S-LA4UyEskrvDnL_ufl_9Qdn656NxwgEMLJ4=",
+      branchOfService: "Navy",
+      county: "PQR County",
+      state: "Florida",
+      online: true,
+      members: [],
+      tags: ["Naval", "Maritime", "Delta"],
+      events: [],
     },
     {
-      id: "4",
-      title: "Outdoor Yoga Session",
-      description:
-        "Experience tranquility and mindfulness with our outdoor yoga classes.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-      buttonText: "Join Session",
+      id: 4,
+      name: "Ranger Squad",
+      description: "Elite Infantry Unit",
+      displayPhoto:
+        "https://media.istockphoto.com/id/1480574582/photo/happy-multigenerational-group-of-women-with-different-ages-and-ethnicities-having-fun-in-a.webp?b=1&s=170667a&w=0&k=20&c=hWNjOWWoiAdq9OLaX4mYfx2rlyQM9CYYfS8aPO6srGY=",
+      branchOfService: "Army",
+      county: "",
+      state: "National",
+      online: false,
+      members: [],
+      tags: ["Infantry", "Special Forces", "Ranger", "Test", "Test", "Test"],
+      events: [],
     },
     {
-      id: "5",
-      title: "Tech Networking Event",
-      description:
-        "Connect with fellow tech enthusiasts and industry leaders at our networking event.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-      buttonText: "Sign Up",
-    },
-    {
-      id: "6",
-      title: "Cooking Workshop",
-      description:
-        "Learn new culinary skills in our interactive cooking workshop.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-      buttonText: "Enroll Today",
-    },
-    {
-      id: "7",
-      title: "Book Club Meetup",
-      description:
-        "Join our monthly book club meeting to discuss this month's chosen book.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-      buttonText: "Attend Meetup",
-    },
-    {
-      id: "8",
-      title: "Live Jazz Night",
-      description: "Enjoy an evening of live jazz music at our local café.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-      buttonText: "Reserve Seats",
-    },
-    {
-      id: "9",
-      title: "Community Gardening",
-      description:
-        "Get your hands dirty and help beautify our community garden.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-      buttonText: "Volunteer",
-    },
-    {
-      id: "10",
-      title: "Film Screening Under the Stars",
-      description:
-        "Join us for an open-air screening of classic and contemporary films.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-      buttonText: "Buy Tickets",
+      id: 5,
+      name: "Echo Company",
+      description: "Communication and Technology",
+      displayPhoto:
+        "https://media.istockphoto.com/id/951541912/photo/happiness-happens-when-we-stand-together.jpg?s=612x612&w=0&k=20&c=R1qXpX5S-LA4UyEskrvDnL_ufl_9Qdn656NxwgEMLJ4=",
+      branchOfService: "Army",
+      county: "JKL County",
+      state: "National",
+      online: true,
+      members: [],
+      tags: ["Communication", "Technology", "Echo"],
+      events: [],
     },
   ];
 
   return (
-    <div className="flex flex-col gap-12">
-      <div className="flex flex-col gap-6">
-        <p className="text-heading3">Groups you might be interested in</p>
-        <div className="flex flex-row gap-8 overflow-scroll overflow-y-hidden">
+    <div className='flex flex-col gap-12'>
+      <div className='flex flex-col gap-6'>
+        <p className='text-heading4'>My Groups</p>
+        <SimpleGrid columns={[1, 2, 3]} spacing={6}>
+          {sampleGroupData.map((group, index) => {
+            return <GroupCard key={index} {...group} />;
+          })}
+        </SimpleGrid>
+      </div>
+      <Divider />
+      <div className='flex flex-col gap-6'>
+        <p className='text-heading3'>Groups you might be interested in</p>
+        <div className='flex flex-row gap-8 overflow-scroll overflow-y-hidden'>
           {sampleGroupData.map((group, index) => {
             return (
-              <div key={index} className="min-w-[330px] sm:min-w-[500px] mb-4">
+              <div key={index} className='min-w-[330px] sm:min-w-[500px] mb-4'>
                 <GroupCard {...group} />
               </div>
             );
           })}
         </div>
-      </div>
-
-      <div className="flex flex-col gap-6">
-        <p className="text-heading4">Explore more groups</p>
-        <SimpleGrid columns={[1, 2, 3, 4]} spacing={8}>
-          {sampleGroupData.map((group, index) => {
-            return <GroupCard key={index} {...group} />;
-          })}
-        </SimpleGrid>
       </div>
     </div>
   );
