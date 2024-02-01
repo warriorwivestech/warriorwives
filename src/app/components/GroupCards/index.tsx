210157bb6e9370e8469f27972d91c0bf68bcb806
nickfarm27 <nicholasfarm27@gmail.com>
2024-02-01 21:09:28 +0000

    Update groups page to call db and add a loading state

--- src/app/components/GroupCards/index.tsx
+++ src/app/components/GroupCards/index.tsx
@@ -31,10 +31,11 @@ interface GroupCardProps {
   description: string;
   displayPhoto: string;
   branchOfService: string;
-  county: string;
+  county: string | null;
   state: string;
   online: boolean;
   tags: string[];
+  admin: boolean;
 }
 
 export default function GroupCard({
@@ -47,33 +48,42 @@ export default function GroupCard({
   state,
   online,
   tags,
+  admin,
 }: GroupCardProps) {
+  const displayPhotoUrl =
+    displayPhoto ||
+    "https://t4.ftcdn.net/jpg/03/40/52/49/360_F_340524914_pzOWCq4I0WjytxaW8DTVFujrck1gjvvO.jpg";
+  const location = county ? `${county}, ${state}` : state;
+  const parsedBranchOfService = branchOfService === "Any" ? "All Branches" : branchOfService;
+
   return (
     <Link href={`/groups/${id}`}>
-      <Card className='w-[100%] h-[100%] transition-all duration-300 ease-in-out hover:bg-gray-50 hover:shadow-lg'>
+      <Card className="w-[100%] h-[100%] transition-all duration-300 ease-in-out hover:bg-gray-50 hover:shadow-lg">
         <CardBody>
-          <HStack justifyContent={"space-between"} marginBottom='0.75rem'>
-            <Text>{branchOfService}</Text>
+          <HStack justifyContent={"space-between"} marginBottom="0.75rem">
+            <Text>{parsedBranchOfService}</Text>
             <IconText
               icon={FaMapMarkerAlt}
-              iconClassName='text-red-700'
-              textClassName='whitespace-nowrap font-semibold'
-            >{`${county && `${county}, `} ${state}`}</IconText>
+              iconClassName="text-red-700"
+              textClassName="whitespace-nowrap font-semibold"
+            >
+              {location}
+            </IconText>
           </HStack>
           <Image
-            src={displayPhoto}
+            src={displayPhotoUrl}
             alt={name}
-            borderRadius='lg'
-            className='w-full object-cover h-60'
+            borderRadius="lg"
+            className="w-full object-cover h-60"
           />
-          <Stack mt='6' spacing='3'>
-            <Flex flexDirection='row' justifyContent='space-between'>
+          <Stack mt="6" spacing="3">
+            <Flex flexDirection="row" justifyContent="space-between">
               <Tags tags={tags} />
               {online && (
-                <Popover trigger='hover' placement='top'>
+                <Popover trigger="hover" placement="top">
                   <PopoverTrigger>
                     <div>
-                      <RiBaseStationLine className='text-[1.5rem] text-green-500 mr-2 ml-2' />
+                      <RiBaseStationLine className="text-[1.5rem] text-green-500 mr-2 ml-2" />
                     </div>
                   </PopoverTrigger>
                   <PopoverContent>
@@ -93,9 +103,8 @@ export default function GroupCard({
               )}
             </Flex>
             {/* name */}
-            <Heading size='md'>{name}</Heading>
-            {/* description */}
-            <Flex textOverflow='ellipsis' noOfLines={5}>
+            <Heading size="md">{name}</Heading>
+            <Flex textOverflow="ellipsis" noOfLines={5}>
               <Text>{description}</Text>
             </Flex>
           </Stack>
