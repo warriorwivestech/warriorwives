db8dd0e7a3faaca0d3661f54c141832bcc62c0c1
nickfarm27 <nicholasfarm27@gmail.com>
2024-03-19 01:45:32 +0800

    Refactor and update 'My Groups' page to fetch data directly from db

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,22 +1,31 @@
 import RecommendedGroups from "../components/RecommendedGroups/RecommendedGroups";
 import GroupCard from "../components/GroupCards";
 import { Divider, SimpleGrid } from "@chakra-ui/react";
-import { JoinedGroupsResponse } from "../api/users/[userId]/groups/route";
-import { apiClient } from "../apiClient";
+import { getJoinedGroups } from "../data/groups";
 
 export default async function GroupsPage() {
   // TODO: update this to use the user's actual id
-  const groups: JoinedGroupsResponse = await apiClient(`/users/3/groups`)
+  const { data: groups, error } = await getJoinedGroups(3);
+
+  const GroupsData = () => {
+    if (error) return <div>Error loading groups</div>;
+    if (!groups || groups.length === 0)
+      return <div>You have not joined any groups yet.</div>;
+
+    return (
+      <SimpleGrid columns={[1, 2, 3]} spacing={6}>
+        {groups.map((group) => {
+          return <GroupCard key={group.id} {...group} />;
+        })}
+      </SimpleGrid>
+    );
+  };
 
   return (
     <div className="flex flex-col gap-12">
       <div className="flex flex-col gap-6">
         <p className="text-heading4">My Groups</p>
-        <SimpleGrid columns={[1, 2, 3]} spacing={6}>
-          {groups.map((group) => {
-            return <GroupCard key={group.id} {...group} />;
-          })}
-        </SimpleGrid>
+        <GroupsData />
       </div>
       <Divider />
       <div className="flex flex-col gap-6">
