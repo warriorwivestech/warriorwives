3cc6e9dcdd342e91c7ee4f1e54a9d3a6807a01c2
nickfarm27 <nicholasfarm27@gmail.com>
2024-04-07 15:04:40 +0800

    Fix intro hydration error

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,67 +1,56 @@
-"use client";
-
-import React from "react";
-import IconText from "../common/icontext";
-import { TbTargetArrow } from "react-icons/tb";
-import { IoRocketSharp } from "react-icons/io5";
-import { Card, CardBody, Flex } from "@chakra-ui/react";
 import { TypographyH1 } from "../ui/typography/h1";
-import { TypographyLarge } from "../ui/typography/large";
-import { TypographyP } from "../ui/typography/p";
-
-const emojis = [
-  "💪",
-  "✨",
-  "🤘",
-  "⚔️",
-  "🪖",
-  "🧗‍♀️",
-  "🏌️",
-  "🏋️‍♀️",
-  "🚴‍♀️",
-  "🏄‍♀️",
-  "🏃‍♀️",
-  "🧘‍♀️",
-];
+import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { TargetIcon, RocketIcon } from "@radix-ui/react-icons";
 
 const Intro = () => {
+  const emojis = [
+    "💪",
+    "✨",
+    "🤘",
+    "⚔️",
+    "🪖",
+    "🧗‍♀️",
+    "🏌️",
+    "🏋️‍♀️",
+    "🚴‍♀️",
+    "🏄‍♀️",
+    "🏃‍♀️",
+    "🧘‍♀️",
+  ];
+
   return (
-    <Flex flexDir="column" gap="1rem">
+    <div>
       <TypographyH1>
         Welcome back to WarriorWives!{" "}
         {emojis[Math.round(Math.random() * emojis.length)]}
       </TypographyH1>
-      <Card background="gray.100">
-        <CardBody>
-          <Flex flexDir="column" gap="2rem">
-            <div>
-              <IconText icon={TbTargetArrow} textClassName="text-lg">
-                <TypographyLarge>Vision</TypographyLarge>
-              </IconText>
-              <TypographyP>
-                To be an outreach organization in support of military wives; To
-                provide prayer, encouragement and fundamental training resources
-                for women that goes beyond the limitations of local, state and
-                federal agencies; To unite women with others in their
-                geographical area, regardless of branch, duty status or unit
-                association; and to promote one another through all stages of
-                military service through faith in Jesus Christ.
-              </TypographyP>
-            </div>
-            <div>
-              <IconText icon={IoRocketSharp} textClassName="text-lg">
-                <TypographyLarge>Mission</TypographyLarge>
-              </IconText>
-              <TypographyP>
-                {
-                  "Preparing military wives for life's daily battles; through prayer, training and relationship-building."
-                }
-              </TypographyP>
-            </div>
-          </Flex>
-        </CardBody>
+      <Card className="bg-gray-100 mt-4">
+        <CardHeader>
+          <CardTitle className="flex">
+            <TargetIcon className="mr-2 h-4 w-4" />
+            <div>Vision</div>
+          </CardTitle>
+          <CardDescription>
+            To be an outreach organization in support of military wives; To
+            provide prayer, encouragement and fundamental training resources for
+            women that goes beyond the limitations of local, state and federal
+            agencies; To unite women with others in their geographical area,
+            regardless of branch, duty status or unit association; and to
+            promote one another through all stages of military service through
+            faith in Jesus Christ.
+          </CardDescription>
+          <CardTitle className="flex pt-4">
+            <RocketIcon className="mr-2 h-4 w-4" />
+            <div>Mission</div>
+          </CardTitle>
+          <CardDescription>
+            {
+              "Preparing military wives for life's daily battles; through prayer, training and relationship-building."
+            }
+          </CardDescription>
+        </CardHeader>
       </Card>
-    </Flex>
+    </div>
   );
 };
 
