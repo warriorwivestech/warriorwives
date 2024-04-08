92d98aedbb3787bd34c0d6aeeec1330d8e6ac161
nickfarm27 <nicholasfarm27@gmail.com>
2024-04-08 22:36:11 +0800

    Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="min-h-[60vh] flex justify-center items-center">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle className="text-center">Page Not Found</CardTitle>
+          <CardDescription className="text-center">
+            Could not find requested resource
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Link href="/">
+            <Button className="w-full">Return Home</Button>
+          </Link>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
