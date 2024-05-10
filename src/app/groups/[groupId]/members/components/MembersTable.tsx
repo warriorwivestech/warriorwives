2cbfd4682c4c2e09f75ce5c68061064ad2b71fd0
nickfarm27 <nicholasfarm27@gmail.com>
2024-05-10 23:32:02 +0800

    Allow group members data to be exported

--- src/app/groups/[groupId]/members/components/MembersTable.tsx
+++ src/app/groups/[groupId]/members/components/MembersTable.tsx
@@ -18,6 +18,8 @@ import { GroupMembers } from "@/app/api/groups/[groupId]/members/route";
 import PromoteModal from "./PromoteModal";
 import RemoveModal from "./RemoveModal";
 import LoadingMembersTable from "./LoadingMembersTable";
+import { CSVLink } from "react-csv";
+import { Button } from "@/components/ui/button";
 
 interface MembersTableProps {
   user: UserDataType;
@@ -49,66 +51,109 @@ function _MembersTables({ user, userIsAdmin, groupData }: MembersTableProps) {
       })
     : [];
 
-  return (
-    <Table className="mt-4">
-      <TableCaption>All members.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="min-w-[100px]">Name</TableHead>
-          <TableHead>Email</TableHead>
-          <TableHead>Role</TableHead>
-          <TableHead>Branch of Service</TableHead>
-          <TableHead className="min-w-[140px]">Joined On</TableHead>
-          <TableHead className="min-w-[240px]">Actions</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {sortedMembers.map((member) => {
-          // a superUser can remove any member (admin or normal members)
-          // admins can remove normal members but not admins
-          const userCanRemove =
-            user.superUser || (userIsAdmin && !member.admin);
-          // admins or superUsers can promote normal members to admin
-          const userCanPromote =
-            user.superUser || (userIsAdmin && !member.admin);
-          // only superUsers can demote admins
-          const userCanDemote = user.superUser;
+  const exportedMembersData = sortedMembers.map((member) => {
+    return {
+      id: member.id,
+      name: member.name,
+      email: member.email,
+      role: member.admin ? "Admin" : "Member",
+      branch: member.branch,
+      joinedOn: parseDate(member.createdAt),
+      facebook: member.facebook,
+      instagram: member.instagram,
+      twitter: member.twitter,
+      linkedin: member.linkedin,
+    };
+  });
 
-          return (
-            <TableRow key={member.id}>
-              <TableCell>{member.name}</TableCell>
-              <TableCell>{member.email}</TableCell>
-              <TableCell>{member.admin ? "Admin" : "Member"}</TableCell>
-              <TableCell>{member.branch}</TableCell>
-              <TableCell>{parseDate(member.createdAt)}</TableCell>
-              <TableCell>
-                {userCanPromote && !member.admin && (
-                  <PromoteModal
-                    member={member}
-                    groupId={groupData.id}
-                    revalidateData={mutate}
-                  />
-                )}
-                {userCanDemote && member.admin && (
-                  <DemoteModal
-                    member={member}
-                    groupId={groupData.id}
-                    revalidateData={mutate}
-                  />
-                )}
-                {userCanRemove && (
-                  <RemoveModal
-                    member={member}
-                    groupId={groupData.id}
-                    revalidateData={mutate}
-                  />
-                )}
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+  const headers = [
+    { key: "id", label: "ID" },
+    { key: "name", label: "Name" },
+    { key: "email", label: "Email" },
+    { key: "role", label: "Role" },
+    { key: "branch", label: "Branch of Service" },
+    { key: "joinedOn", label: "Joined On" },
+    { key: "facebook", label: "Facebook" },
+    { key: "instagram", label: "Instagram" },
+    { key: "twitter", label: "Twitter" },
+    { key: "linkedin", label: "LinkedIn" },
+  ];
+
+  return (
+    <>
+      <div className="flex justify-end">
+        <CSVLink
+          data={exportedMembersData}
+          headers={headers}
+          filename={`members-${groupData.name}.csv`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button size="sm" variant="outline">
+            Export Data
+          </Button>
+        </CSVLink>
+      </div>
+      <Table className="mt-4">
+        <TableCaption>All members.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="min-w-[100px]">Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Role</TableHead>
+            <TableHead>Branch of Service</TableHead>
+            <TableHead className="min-w-[140px]">Joined On</TableHead>
+            <TableHead className="min-w-[240px]">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {sortedMembers.map((member) => {
+            // a superUser can remove any member (admin or normal members)
+            // admins can remove normal members but not admins
+            const userCanRemove =
+              user.superUser || (userIsAdmin && !member.admin);
+            // admins or superUsers can promote normal members to admin
+            const userCanPromote =
+              user.superUser || (userIsAdmin && !member.admin);
+            // only superUsers can demote admins
+            const userCanDemote = user.superUser;
+
+            return (
+              <TableRow key={member.id}>
+                <TableCell>{member.name}</TableCell>
+                <TableCell>{member.email}</TableCell>
+                <TableCell>{member.admin ? "Admin" : "Member"}</TableCell>
+                <TableCell>{member.branch}</TableCell>
+                <TableCell>{parseDate(member.createdAt)}</TableCell>
+                <TableCell>
+                  {userCanPromote && !member.admin && (
+                    <PromoteModal
+                      member={member}
+                      groupId={groupData.id}
+                      revalidateData={mutate}
+                    />
+                  )}
+                  {userCanDemote && member.admin && (
+                    <DemoteModal
+                      member={member}
+                      groupId={groupData.id}
+                      revalidateData={mutate}
+                    />
+                  )}
+                  {userCanRemove && (
+                    <RemoveModal
+                      member={member}
+                      groupId={groupData.id}
+                      revalidateData={mutate}
+                    />
+                  )}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </>
   );
 }
 
